a8d3ed4d4e5ad21705b6e2305b31cfcde5fb635a
spydiewraith <spydiewraith@contractor.net>
2024-09-19 20:58:59 +0200

    Python Exercises 9/19/24 Day 1

---
+++ length.py
@@ -0,0 +1,10 @@
+name = "Jack"
+print(len(name))
+
+name = "Angela"
+print(len(name))
+
+
+username = input("What is your name?")
+length = len(username)
+print(length)
---
+++ variable.py
@@ -0,0 +1,3 @@
+name = input("Hello, what is your name? ")
+print(name)
+
